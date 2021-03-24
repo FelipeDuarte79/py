8e69be6f1d905c00b8545a15c5697c89e407b2eb
Felipe Duarte <felipe.duarte79@protonmail.com>
2021-03-23 22:18:33 -0300

    entrada de dados com Python2

---
+++ input/input2.py
@@ -0,0 +1,15 @@
+# -*- coding: utf8 -*-
+
+'''
+#### ENTRADA DE DADOS ####
+- Python2.x tem uma função integrada, que interrompe o programa para que usuário
+entre com um dado
+- a função raw_input sempre retornará uma string
+- o nome da função para a entrada de dados no Python2.x é raw_input
+- o conteúdo dentro dos parênteses (argumento da função) é um texto que você
+pode dirigir ao usuário, advertindo o que você espera que ele digite
+'''
+
+# Exemplo
+
+inteiros = raw_input('Digite um valor inteiro: ')
