32201917c5043640105472c5aa15b6b9d7d7be0d
Felipe Duarte <felipe.duarte79@protonmail.com>
2021-03-23 22:18:46 -0300

    entrada de dados com Python3

---
+++ input/input3.py
@@ -0,0 +1,15 @@
+# -*- coding: utf8 -*-
+
+'''
+#### ENTRADA DE DADOS ####
+- Python3.x tem uma função integrada, que interrompe o programa para que usuário
+entre com uma informação
+- a função input sempre retornará uma string
+- o nome da função para a entrada de dados no Python3.x é input
+- o conteúdo dentro dos parênteses (argumento da função) é um texto que você
+pode dirigir ao usuário, advertindo o que você espera que ele digite
+'''
+
+# Exemplo
+
+inteiros = input('Digite um valor inteiro: ')
