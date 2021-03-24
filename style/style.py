14760a3426b2f77ac839d1ca13cf900a9975bd49
Felipe Duarte <felipe.duarte79@protonmail.com>
2021-03-23 22:35:27 -0300

    estilos de programação

---
+++ style/style.py
@@ -0,0 +1,26 @@
+# -*- coding: utf8 -*-
+
+'''
+#### COMENTÁRIOS ####
+- Python ignora comentários
+- utilize para descrever notas, observações
+- tudo o que for escrito após o caractere será ignrado
+- pode utilizar este recurso como comentário de uma ou meia linha
+- pode utilizar # para comentar trechos inteiros e assim testar partes do código
+- pode gerar uma documentação de várias linhas utilizando três aspas simples oU
+duplas
+- este recurso também pode ser exibido na tela como documentação para o usuário
+'''
+
+# Exemplo
+
+
+doc = '''\n\nExemplo
+de      uma    
+             docu-
+men-
+           tação com várias linhas.'''
+
+print(type(doc))
+print(doc)
+ 
