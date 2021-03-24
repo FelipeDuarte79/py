0d05792a33f54c9cf5bb57e6a533ee43dccbeab8
Felipe Duarte <felipe.duarte79@protonmail.com>
2021-03-23 22:11:52 -0300

    tipo e operadores booleanos

---
+++ bool/bool.py
@@ -0,0 +1,41 @@
+# -*- coding: utf8 -*-
+
+'''
+#### TIPO BOOLEANO ####
+_ valores booleano possuem apenas um valor de dua opções: verdadeiro ou falso
+- True (verdadeiro)
+- False falso)
+
+#### OPERADORES BOOLEANOS ####
+- and (e)
+- or (ou)
+- not ((negação)
+
+#### OPERADORES RELACIONAIS ####
+- ==  (igual)
+- !=  (diferente)
+- <   (menor que)
+- <=  (menor ou igual)
+- >   (maior que)
+- >=  (maior ou igual)
+'''
+
+# Exemplos
+
+print(not True)
+print(not False)
+print(True and True)
+print(True and False)
+print(False and False)
+print(True or True)
+print(True or False)
+print(False or False)
+
+print(True == True)
+print(False == True)
+print(True != True)
+print(False != True)
+print(True < True)
+print(True <= True)
+print(False > False)
+print(True >= False)
