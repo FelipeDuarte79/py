4fe8df07d102d5244fb30b19f61086acac48ebaf
Felipe Duarte <felipe.duarte79@protonmail.com>
2021-03-23 22:40:03 -0300

    variaveis

---
+++ var/var.py
@@ -0,0 +1,28 @@
+# -*- coding: utf8 -*-
+
+'''
+#### VARIÁVEIS ####
+- variável é um nome que se refere a um valor
+- de forma análoga, podemos comparar com um caixa ou gaveta que armazena um dado
+- a variável pode armazenar apenas uma informação por vez
+- assim, um dado que é subscrito é perdido e não poderá ser utilizado novamente
+# Nomes de Variáveis
+- escolha nomes significativos, autoexplicativos e sucintos
+- nomes de variáveis podem ser tão longos, quanto você precisar
+- pode ser tedioso escrever nomes muito grandes
+- o primeiro caractere deve ser obrigatoriamente uma letra
+- as demais letras podem conter letras, números e sublinhados
+- por padrão, utilize apenas caractere minúsculos
+- _ pode ser usado em nomes de variáveis que possuem dois ou mais nomes 
+- escolha um estilo camelcase (camelCase ou CamelCase) ou snake_case e utilize
+como padrão 
+- escolha nomes coletivos para variaveis genericas, listas etc
+- deixe nomes no singular para variaveis específicas
+# Atribuindo valores
+- = (operador de atribuição): na esquerda o seu nome, direita fica o valor
+'''
+
+# Exemplos
+inteiro = 0                   # atribuição simples
+int0 = int1 = int2 = 0        # atribuição múltipla de mesmo valor
+int0, int1, int2 = 0, 1, 2    # atribução múltipla de diferentes valores
