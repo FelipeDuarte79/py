bb95d64bf473daec81ddb80f8ebdc2e88f547ff7
Felipe Duarte <felipe.duarte79@protonmail.com>
2021-03-23 22:22:06 -0300

    obtendo o tamanho

---
+++ len/len.py
@@ -0,0 +1,17 @@
+# -*- coding: utf8 -*-
+
+'''
+#### DESCOBRINDO O TAMANHO ####
+- é possível obter o tamanho de um dado (string, número, variavel, docstring)
+com a função len
+- len retorna o tamanho de caracteres (e não de bytes)
+'''
+
+# Exemplo
+
+print(len('Python'))
+var = ''
+print(len(var))
+print(len('''docu
+men
+tação'''))
