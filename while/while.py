4e66d8e38c27472ed48a9c043cd88d6f86a266bd
Felipe Duarte <felipe.duarte79@protonmail.com>
2021-03-26 11:22:09 -0300

    laço de repetiçao while

---
+++ while/while.py
@@ -0,0 +1,13 @@
+# -*- coding: utf8 -*-
+
+'''
+#### LAÇO DE REPETIÇÃO WHILE ####
+- um bloco de código pode ser executado repetidamente
+- a cláusula while será executada indefibidamente enquanto a condição for True
+- a instrução break encerra imediatamente o laço
+- a instrução continue pula para o próximo laço
+- a instrução pass apenas passa sem fazer nada (útil para futuras implementações
+elif que não foram completamente implementadas)
+'''
+
+# Exemplo
